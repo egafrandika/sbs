81f196f20b28409598cd416111b8cf3faa60c4a3
egafrandika <ega.frandika@fintopia.tech>
2025-03-11 16:08:07 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import API_BASE_URL from './src/services/axios/stages';
+// import API_BASE_URL from './src/services/axios/stages';
 
 export default defineConfig(({ mode }) => {
   // Load environment variables
@@ -27,7 +27,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: API_BASE_URL,
+          target: env.VITE_API_BASE_URL || 'http://103.63.25.188:9000',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
