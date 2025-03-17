a4a4fc52973abbc422741f50c345a53b69e078d2
egafrandika <ega.frandika@fintopia.tech>
2025-03-17 15:41:12 +0700

    fix

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-home">
-        <p class="line-1 anim-typewriter">
+        <p>
             WELCOME TO SBS
         </p>
     </div>
@@ -20,40 +20,5 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-}
-
-.line-1{
-    position: relative;
-    top: 50%;  
-    width: 24em;
-    margin: 0 auto;
-    border-right: 2px solid rgba(255,255,255,.75);
-    font-size: 180%;
-    text-align: center;
-    white-space: nowrap;
-    overflow: hidden;
-    transform: translateY(-50%);    
-}
-
-/* Animation */
-.anim-typewriter{
-  animation: typewriter 4s steps(44) 1s 1 normal both,
-             blinkTextCursor 500ms steps(44) infinite normal;
-}
-@keyframes typewriter{
-    from {
-        width: 0;
-    }
-    to {
-        width: 24em;
-    }
-}
-@keyframes blinkTextCursor{
-    from{
-        border-right-color: rgba(255,255,255,.75);
-    }
-    to {
-        border-right-color: transparent;
-    }
 }
 </style>
